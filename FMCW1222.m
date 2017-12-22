94f438ba89aed55681576f0c897805f51d5deef3
Finn <spongebob8312@iCloud.com>
2017-12-22 16:29:26 +0800

    still got some issues, "while end" function didn't end properly

--- FMCW1222.m
+++ FMCW1222.m
@@ -1,11 +1,9 @@
-%% Create the serial object
+%% Create serial object
 clear
 clc
 delete(instrfindall);
-s = serial('/dev/tty.usbmodem1431', 'BaudRate', 115200);
-%s = Bluetooth('EE704', 1);
+s = serial('/dev/cu.usbmodem1431', 'BaudRate', 115200);
 fopen(s);
-
 
 a = 'b';
 while (a ~= 'a')
@@ -17,50 +15,78 @@ end
 fprintf(s, '%c', 'a');
 disp('Serial Communication setup.');
 
-%% Sampling Config
+ %% Sampling Config
 Fsamp = 500;            % Sampling frequency                    
 T = 1/Fsamp;             % Sampling period       
 L = 250;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 signal = eye(1,L);
-
-
+    
 composedSignal_length = 2000;  
 composedSignal = eye(1, composedSignal_length);
 segments = composedSignal_length / L;
 composedSignalTime = (0:segments*L-1)*T;
+    
 
 %% Setup fig
 subplot(2,2,1);
 haxes = plot(0 , 0);
 pause(1);
-
-
+%% plot
 while ishandle(haxes)
-
-    % Collect data
+    
+    % retrive signal
     for i=1:L  
-       signal(i) = fscanf(s, '%d');
+        signal(i) = fscanf(s, '%d');
     end
+    
     composedSignal = circshift(composedSignal,-L);
     composedSignal((segments-1)*L+1:segments*L) = signal(1:L);
-    
+    composedSignal = detrend(composedSignal);
+    % plot 4 secs signal
     subplot(2,2,1);
     set(haxes, 'XData', 1000*composedSignalTime(1:composedSignal_length), 'YData', composedSignal(1:composedSignal_length));
     title('Signal')
     xlabel('t (milliseconds)')
     ylabel('signal(t)')
-    ylim([140, 180])
-    
-    
-    %obj_distance = (100 * 3 * 10^8 * 0.4675 * 2 * f / (4 * 394 * 10^6)) - 20;
-    %disp(['distance = ' num2str(round(obj_distance(loc))) 'cm']);
+
+    subplot(2,2,2)
+    IIR = filter(IIRButterworthHighPass, composedSignal);
+    [IIRSig, f] = freqDomain(Fsamp, IIR, L);
+    plot(f, IIRSig);
+    xlim([0 20]);
+    title('IIR');
+    ylabel('|P|');
+    xlabel('Freq(Hz)');
+
+    subplot(2,2,3)
+    FIR = filter(FIRChebysevHP, composedSignal);
+    [FIRSig, f] = freqDomain(Fsamp, FIR, L);
+    plot(f, FIRSig);
+    xlim([0 20]);
+    title('FIR');
+    ylabel('|P|');
+    xlabel('Freq(Hz)');
+
+    subplot(2,2,4)
+    plot(f, IIRSig, f, FIRSig);
+    legend('IIR','FIR')
+    xlim([0 20]);
+    title('FIR & IIR Comparison');
+    ylabel('|P|');
+    xlabel('Freq(Hz)');
+
+    [M, loc] = max(IIRSig);
+    obj_distance = (100 * 3 * 10^8 * 0.4675 * 2 * f / (4 * 394 * 10^6)) - 20;
+    disp(['distance = ' num2str(round(obj_distance(loc))) 'cm']);
  
-    %txt1 = [num2str(f(loc)) 'Hz\rightarrow'];
-    %text(f(loc),LPFSignal(loc),txt1,'HorizontalAlignment','right');
+    txt1 = [num2str(f(loc)) 'Hz\rightarrow'];
+    text(f(loc),IIRSig(loc),txt1,'HorizontalAlignment','right');
     
+
+    pause(0.04); %給足夠時間繪圖不然會崩
     
-    pause(0.04); % give it some time to plot 
+ 
 end
 fclose(s);
-
+ 
